bb47ba0522fcca7166bf51a4e7b698b7d6ac9856
wookyoon <wookyoon.jeung@gmail.com>
2022-05-04 17:32:39 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
+
+import Content from './components/main/Content';
+import Visual from './components/main/Visual';
+
+import Community from './components/sub/Community';
+import Department from './components/sub/Department';
+import Gallery from './components/sub/Gallery';
+import Youtube from './components/sub/Youtube';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <h1>HELLO</h1>;
 }
 
 export default App;
---
+++ src/components/main/Visual.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Visual() {
+	return (
+		<div>
+			<h1>Visual</h1>
+		</div>
+	);
+}
+
+export default Visual;
